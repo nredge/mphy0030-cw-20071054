9c37884e7bd189251793c098399b558c6d3912cc
Nat Redgewell <73437229+nredge@users.noreply.github.com>
2021-01-01 23:41:59 +0000

    Task 3 Update

--- part1/task3/gradient_descent.m
+++ part1/task3/gradient_descent.m
@@ -1,4 +1,17 @@
-function optimal = gradient_descent(quad, x, step_size, max_it, tol, grad)
+function optimal = gradient_descent(quad, x, a, h, max_it, tol)
+it = 1;
+delta = 1;
+while it <= max_it && delta >= tol
+grad = finite_difference_gradient(x,a);
+% grad_norm = norm(grad);
 
-grad_norm = norm(grad);
+newx = x - h * grad;
 
+delta = quad(x,a) - quad (newx,a); 
+
+x = newx;
+
+it = it + 1;
+
+end
+optimal = x;
--- part1/task3/task.m
+++ part1/task3/task.m
@@ -1,17 +1,19 @@
 a = randperm(10);
-a = a';
+% a = a';
 %
-x = [10 10 10];
-x = x';
+x = [2 2 2];
+% x = x';
 
 %%
-quad = @(x1,x2,x3,a) a(2)*x1^2 + a(3)*x2^2 + a(4)*x3^2 + a(5)*x1*x2 + a(6)*x1*x3...
-    + a(7)*x2*x3 + a(8)*x1 + a(9)*x2 + a(10)*x3 + a(1);
+quad = @(x,a) a(2)*x(1)^2 + a(3)*x(2)^2 + a(4)*x(3)^2 + a(5)*x(1)*x(2) + a(6)*x(1)*x(3)...
+    + a(7)*x(2)*x(3) + a(8)*x(1) + a(9)*x(2) + a(10)*x(3) + a(1);
 
-h = 0.01;
-max_it = 100;
-tol = 0.1;
+h = 0.001;
+max_it = 1000;
+tol = 0.01;
 %
-grad = finite_difference_gradient(quad,x,a,h);
-%% 
-gradient_descent(quad,x,a,h,max_it,tol,grad); 
+%grad = finite_difference_gradient(quad,x,a,h);
+%
+test = gradient_descent(quad,x,a,h,max_it,tol); 
+
+final = quad(test,a);
