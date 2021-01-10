cff789d41aa6cb8e1c7d9d780a07185a073cff32
Nat Redgewell <73437229+nredge@users.noreply.github.com>
2021-01-10 23:41:07 +0000

    Part 2 Update

--- part2/FreeFormDeformation.m
+++ part2/FreeFormDeformation.m
@@ -7,21 +7,33 @@ classdef FreeFormDeformation
     end
     
     methods
-        function obj = untitled6(num,range)
+        function control = untitled6(num,range)
             %UNTITLED6 Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = linspace(range(1,1),range(1,2),num(1));
-            obj.Property2 = linspace(range(2,1),range(2,2),num(2));
-            obj.Property3 = linspace(range(3,1),range(3,2),num(3));
+            x = linspace(range(1,1),range(1,2),num(1));
+            y = linspace(range(2,1),range(2,2),num(2));
+            z = linspace(range(3,1),range(3,2),num(3));
+            
+            control = zeros(max(num),3);
+            for ii = 1:max(num)
+               control(ii,:) = [x(ii),y(ii),z(ii)]; 
+            end
         end
         
-        function optional_obj = un(image3d.obj,num_control)
+        function controlimg = un(image3d,num)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+            x = linspace(image3d.range(1,1),image3d.range(1,2),num(1));
+            y = linspace(image3d.range(2,1),image3d.range(2,2),num(2));
+            z = linspace(image3d.range(3,1),image3d.range(3,2),num(3));
+            
+            controlimg = zeros(max(num),3);
+            for ii = 1:max(num)
+               controlimg(ii,:) = [x(ii),y(ii),z(ii)]; 
+            end
         end
         
-        function random = random_transform_generator()
+        function rdm_control = random_transform_generator()
             
         end
         
--- part2/Image3D.m
+++ part2/Image3D.m
@@ -3,14 +3,29 @@ classdef Image3D
     %   Detailed explanation goes here
     
     properties
-        Property1
+        sz
+        range
     end
     
     methods
-        function obj = untitled6(
+        function image3d = untitled6(vol,voxdims)
             %UNTITLED6 Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+            image3d.sz = size(vol);
+            image3d.ind = numel(vol);
+            [row,col,slice] = ind2sub(image3d.sz,1:image3d:ind);
+            points = zeros(image3d.ind,3);
+            for ii = 1:image3d.ind
+                points(ii,:) = [row(ii)*voxdims(1),col(ii)*voxdims(2),slice(ii)*voxdims(3)];
+            end
+            min_x = min(points(:,1);
+            max_x = max(points(:,1);
+            min_y = min(points(:,2);
+            max_y = max(points(:,2);
+            min_z = min(points(:,3);
+            max_z = max(points(:,3);
+            image3d.range = [min_x,max_x;min_y,max_y;min_z,max_z];
+           
         end
         
         
