e54b83eff7bfb38155dd3f6cb915eb32cad83f20
Nat Redgewell <73437229+nredge@users.noreply.github.com>
2021-01-21 14:27:10 +0000

    Task 4 Update

--- part1/task4/lowpass_mesh_smoothing.m
+++ part1/task4/lowpass_mesh_smoothing.m
@@ -23,42 +23,69 @@ dist = zeros(n,1);
 smoothed = zeros(642,3);
 
 for ii = 1:iterations
+    for jj = 1:n
+        
+    tri_ind = find(any(triangles==jj,2));
+    tri = triangles(tri_ind,:);
+    points = unique(tri);
+    points(points == jj) = []; % removes point that is being transformed
+    nn = numel(points); % number of neighbours
+
+    neighbours = vertices(points,:);
+
+    summation =  (1/nn)*(sum(neighbours(:,:) - vertices(jj,:)));
     
-    for jj = 1:n
-        
-        for kk = 1:n
-        dist(kk,:) = norm(vertices(kk,:) - vertices(jj,:)); 
-        end
-        
-        [d,ind] = sort(dist);
-        ind_closest = ind(2:k+1);
-        vertices_closest =  vertices(ind_closest,:);
-        
-        for ll = 1:k
-            summ = summ + (1/k)*(vertices_closest(ll,:) - vertices(jj,:));
-        end
-        
-        vertices(jj,:) = vertices(jj,:) + lambda * summ; 
-        summ(:,:) = 0;
-    end 
-   
-    for jj = 1:n
-        
-        for kk = 1:n 
-            dist(kk,:) = norm(vertices(kk,:) - vertices(jj,:));
-        end
-        
-        [d,ind] = sort(dist); 
-        ind_closest = ind(2:k+1); 
-        vertices_closest =  vertices(ind_closest,:);
-        
-        for ll = 1:k
-            summ = summ + (1/k)*(vertices_closest(ll,:) - vertices(jj,:));
-        end
-        
-        smoothed(jj,:) = vertices(jj,:) + mu *  summ; 
-        summ(:,:) = 0;
+    vertices(jj,:) = vertices(jj,:) + lambda*summation;
+%     end
+%     for jj = 1:n
+    summation =  (1/nn)*(sum(neighbours(:,:) - vertices(jj,:)));
+    
+    vertices(jj,:) = vertices(jj,:) + mu*summation;
+    smoothed(jj,:) = vertices(jj,:);
     end
 end
+    
+    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% for ii = 1:iterations
+%     
+%     for jj = 1:n
+%         
+%         for kk = 1:n
+%         dist(kk,:) = norm(vertices(kk,:) - vertices(jj,:)); 
+%         end
+%         
+%         [d,ind] = sort(dist);
+%         ind_closest = ind(2:k+1);
+%         vertices_closest =  vertices(ind_closest,:);
+%         
+%         for ll = 1:k
+%             summ = summ + (1/k)*(vertices_closest(ll,:) - vertices(jj,:));
+%         end
+%         
+%         vertices(jj,:) = vertices(jj,:) + lambda * summ; 
+%         summ(:,:) = 0;
+%     end 
+%    
+%     for jj = 1:n
+%         
+%         for kk = 1:n 
+%             dist(kk,:) = norm(vertices(kk,:) - vertices(jj,:));
+%         end
+%         
+%         [d,ind] = sort(dist); 
+%         ind_closest = ind(2:k+1); 
+%         vertices_closest =  vertices(ind_closest,:);
+%         
+%         for ll = 1:k
+%             summ = summ + (1/k)*(vertices_closest(ll,:) - vertices(jj,:));
+%         end
+%         
+%         vertices(jj,:) = vertices(jj,:) + mu *  summ; 
+%         smoothed(jj,:) = vertices(jj,:);
+%           summ(:,:) = 0;
+%     end
+% end
 
     
--- part1/task4/task.m
+++ part1/task4/task.m
@@ -4,17 +4,27 @@
 vertices = readmatrix('data/example_vertices.csv');
 triangles = readmatrix('data/example_triangles.csv');
 
+
+
+%%
 iterations = [5 10 25];
-% smoothed = lowpass_mesh_smoothing(vertices,triangles,5);%,10,0.9,-1.02);
-%
+% smoothed = lowpass_mesh_smoothing(vertices,triangles,25);%,10,0.9,-1.02);
+str = string(iterations); 
 figure;
 for ii = 1:size(iterations,2)
   
     smoothed = lowpass_mesh_smoothing(vertices,triangles,iterations(ii));
     
-    subplot(2,2,ii);
+    subplot(2,2,1)
+    trimesh(triangles,vertices(:,1),vertices(:,2),vertices(:,3));
+    title('No smoothing');
+    
+    subplot(2,2,(ii+1));
     trimesh(triangles,smoothed(:,1),smoothed(:,2),smoothed(:,3));
-%     trimesh(triangles,vertices(:,1),vertices(:,2),vertices(:,3));
+    title(sprintf('Smoothing after %i iterations',iterations(ii)));
 end
+
+% figure;
+% trimesh(triangles,vertices(:,1),vertices(:,2),vertices(:,3));
 % figure;
 % trimesh(triangles,smoothed(:,1),smoothed(:,2),smoothed(:,3));
